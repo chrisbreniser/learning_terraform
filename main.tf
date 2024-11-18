d38d407020e176dd6631f6a35a634b8ead89e519
chrisbreniser <73477940+chrisbreniser@users.noreply.github.com>
2024-11-18 15:39:18 -0600

    test revert

--- main.tf
+++ main.tf
@@ -22,25 +22,11 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning more terraform"
   }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name = "blog_new"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress_rules         = ["http-80-tpc","https-443-tcp"]
-  ingress_cidr_blocks  = ["0.0.0.0/0"]
-  
-  egress_rules         = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
