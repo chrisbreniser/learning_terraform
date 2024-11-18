c5e553a531c20b8de1400f02d3b0bb6164a7d130
chrisbreniser <73477940+chrisbreniser@users.noreply.github.com>
2024-11-18 15:24:36 -0600

    made custom module for security group

--- main.tf
+++ main.tf
@@ -22,12 +22,27 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning more terraform"
   }
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules         = ["http-80-tpc","https-443-tcp"]
+  ingress_cider_blocks  = ["0.0.0.0/0"]
+  
+  egress_rules         = ["all-all"]
+  egress_cider_blocks  = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "blog" {
   name = "blog"
@@ -45,6 +60,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 
   security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
